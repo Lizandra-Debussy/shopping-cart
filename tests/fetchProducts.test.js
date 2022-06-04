60cb219b370d76bc9f897f3c79b488ca5b965cd1
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-06-04 16:43:49 -0300

    Requisito 1 concluído

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,31 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function')
+  });
+  
+  it('Com o argumento computador, a função fetch deve ser chamada', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Com o argumento computador, a função fetch utiliza o endpoint', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+  
+  it('Se o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    expect.assertions(1);
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+
+  it('Se ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+    expect.assertions(1);
+    expect(await fetchProducts()).toEqual('You must provide an url');
+  });
+  
 });
